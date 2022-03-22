bca9f1e8ac4c81218916cfeb3c09c015141dd636
zhouzhipeng <admin@zhouzhipeng.com>
2022-03-22 10:35:51 +0800

    http & file combiled to one server

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
 
 ```
+
+
+## TODO
+
+- [ ] go embed 文件直接读取 (主要用于展示首页框架页面)
+- [ ] iframe 页面结构 
+- [ ] js端封装通用ajax请求方法。 js--> go
+- [ ] js端vue做数据绑定渲染
+- [ ] go端简单通用的crud
--- main.go
+++ main.go
@@ -10,84 +10,39 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"sync"
 )
 
 //go:embed www
-var fs embed.FS
+var www embed.FS
 
-// Go types that are bound to the UI must be thread-safe, because each binding
-// is executed in its own goroutine. In this simple case we may use atomic
-// operations, but for more complex cases one should use proper synchronization.
-type counter struct {
-	sync.Mutex
-	count int
-}
-
-func (c *counter) Add(n int) {
-	c.Lock()
-	defer c.Unlock()
-	c.count = c.count + n
-}
-
-func (c *counter) Value() int {
-	c.Lock()
-	defer c.Unlock()
-	return c.count
-}
+//go:embed www/index.html
+var indexHtml string
 
 func main() {
 	args := []string{}
 	if runtime.GOOS == "linux" {
-		args = append(args, "--class=Lorca")
+		args = append(args, "--class=gogo")
 	}
-	ui, err := lorca.New(`data:text/html,
-			<html>
-				<head>
-					<title>GoGo!</title>
-				</head>
-				<body>
-					<h1 style="text-align:center;vertical-align:middle">Loading...</h1>
-					<script type="text/javascript">
-					//window.location.href="http://127.0.0.1:59989/www"
-window.addEventListener('beforeunload',async function (e) {
-    e.preventDefault();
-    e.returnValue = '';
-	console.log("browser close...");
-	await fetch("http://127.0.0.1:8000/window-close")
-});
-					</script>
-				</body>
-			</html>`,
-		"", 480, 320, args...)
+	ui, err := lorca.New("data:text/html,"+indexHtml, "", 480, 320, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ui.Close()
 
-	//bind browser window close event;
-
-	ln_http, err_http := net.Listen("tcp", "127.0.0.1:8000")
-	if err_http != nil {
-		log.Fatal(err_http)
-	}
-	defer ln_http.Close()
-	go http.Serve(ln_http, nil)
-	http.HandleFunc("/window-close", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
+	//http & file server
+	http.Handle("/", http.FileServer(http.FS(www)))
+	http.HandleFunc("/api/window-close", func(w http.ResponseWriter, r *http.Request) {
+		//bind browser window close event;
+		fmt.Fprintf(w, "ok.")
 		ui.Close()
 	})
 
-	// Load HTML.
-	// You may also use `data:text/html,<base64>` approach to load initial HTML,
-	// e.g: ui.Load("data:text/html," + url.PathEscape(html))
-
-	ln, err := net.Listen("tcp", "127.0.0.1:59989")
+	ln, err := net.Listen("tcp", "127.0.0.1:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ln.Close()
-	go http.Serve(ln, http.FileServer(http.FS(fs)))
+	go http.Serve(ln, nil)
 
 	// Wait until the interrupt signal arrives or browser window is closed
 	sigc := make(chan os.Signal)
