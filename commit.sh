229588651402e8f88419c4e574c94ea3897aa2f3
zhouzhipeng <823143047@qq.com>
2022-03-31 22:56:50 +0800

    add win

--- commit.sh
+++ commit.sh
@@ -5,13 +5,13 @@ set -eux
 # start build python exe file
 cd pytool
 
-if [ "$(uname)"=="Darwin" ] ; then
+if [ "$(uname)" == "Darwin" ] ; then
 # Mac OS X 操作系统
 ./buildMac.sh
-elif [ "$(expr substr $(uname -s) 1 5)"=="Linux" ] ; then
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
 # GNU/Linux操作系统
 echo "not supported yet!"
-elif [ "$(expr substr $(uname -s) 1 10)"=="MINGW32_NT" ] ; then
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ] ; then
 # Windows NT操作系统
 ./buildWin.sh
 fi
