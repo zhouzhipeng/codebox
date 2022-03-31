dc0b9297367782264a1267a5f3963cf94076c959
zhouzhipeng <admin@zhouzhipeng.com>
2022-03-31 21:55:05 +0800

    add python build scripts

--- pytool/buildMac.sh
+++ pytool/buildMac.sh
@@ -1,4 +1,5 @@
-rm -rf build dist
+rm -rf web.spec build dist
 
-pip install py2app
-python setup.py py2app
+pip install pyinstaller
+
+pyinstaller  --noconfirm  --console   --log-level "DEBUG"  -c -F   --add-data "views:views" --add-data "static:static" -p /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages   web.py
