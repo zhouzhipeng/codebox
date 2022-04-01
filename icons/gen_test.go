12a8ae1a338a0f8fd9d18ba7484c3b679478b9de
zhouzhipeng <823143047@qq.com>
2022-04-01 21:50:51 +0800

    clean

--- icons/gen_test.go
+++ icons/gen_test.go
@@ -11,7 +11,7 @@ func TestGenIconWin(t *testing.T) {
 
 	var totalBytes uint64
 
-	var outputStr = ""
+	var outputStr = "//go:build windows\n// +build windows\n"
 
 	outputStr += fmt.Sprintf("package %s\n\n", "icons")
 	outputStr += fmt.Sprintf("var %s []byte = []byte{", "Data")
@@ -34,4 +34,33 @@ func TestGenIconWin(t *testing.T) {
 	fmt.Println(outputStr)
 
 	os.WriteFile("iconwin.go", []byte(outputStr), 0777)
+}
+
+func TestGenIconUnix(t *testing.T) {
+
+	var totalBytes uint64
+
+	var outputStr = "//go:build linux || darwin \n// +build linux darwin\n"
+
+	outputStr += fmt.Sprintf("package %s\n\n", "icons")
+	outputStr += fmt.Sprintf("var %s []byte = []byte{", "Data")
+	bytes, err := os.ReadFile("icon-256.png")
+
+	if err != nil && err != io.EOF {
+		fmt.Errorf("Error: %v", err)
+		return
+	}
+	for _, b := range bytes {
+		if totalBytes%12 == 0 {
+			outputStr += fmt.Sprintf("\n\t")
+		}
+		outputStr += fmt.Sprintf("0x%02x, ", b)
+		totalBytes++
+	}
+
+	outputStr += fmt.Sprintf("\n}\n\n")
+
+	fmt.Println(outputStr)
+
+	os.WriteFile("iconunix.go", []byte(outputStr), 0777)
 }
