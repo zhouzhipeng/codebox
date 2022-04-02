3729a0ac7d05ad7a5102dbede719d833adc4ae6c
zhouzhipeng <823143047@qq.com>
2022-04-02 18:28:38 +0800

    update

--- commit.sh
+++ commit.sh
@@ -4,30 +4,24 @@ set -eux
 #rm -rf dist
 
 
-if [ "$(git status  | grep pytool)" != "" ] || [ "$2" == "f" ] ; then
-  echo "pytool change"
 
 
-  # start build python exe file
-  cd pytool
+# start build python exe file
 
-  if [ "$(uname)" == "Darwin" ] ; then
-  # Mac OS X 操作系统
-  ./buildMac.sh
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
-  # GNU/Linux操作系统
-  echo "not supported yet!"
-  elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ] ; then
-  # Windows NT操作系统
-  ./buildWin.sh
-  fi
-  cd ..
 
-  # end build python exe file
-
+if [ "$(uname)" == "Darwin" ] ; then
+# Mac OS X 操作系统
+./build-macos.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
+# GNU/Linux操作系统
+echo "not supported yet!"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ] ; then
+# Windows NT操作系统
+./build-windows.sh
 fi
 
 
+# end build python exe file
 
 echo "$(date +%F\ %T)" >  static/time.txt
 
