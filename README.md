4f677225bcdf67e9d7a88d5fb3bab4762cbf9911
zhouzhipeng <823143047@qq.com>
2022-04-02 18:04:46 +0800

    update

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
 - [x] dropdown menu :  https://www.runoob.com/try/try.php?filename=bootstrap3-dropdown-basic&basepath=0
 - [x] sql runner page see : https://www.runoob.com/try/try.php?filename=bootstrap3-dropdown-basic&basepath=0  (page structure) 
 - [ ] 文件拖拽直接打开 （自动识别文本和二进制)
+- [ ] win box https://github.com/nextapps-de/winbox
 
 ## others
 go template: http://books.studygolang.com/gowebexamples/templates/ 
--- main.go
+++ main.go
@@ -81,73 +81,15 @@ func onReady() {
 	// We can manipulate the systray in other goroutines
 	go func() {
 		systray.SetTemplateIcon(icons.Data, icons.Data)
-		systray.SetTitle("Awesome App")
-		systray.SetTooltip("Pretty awesome棒棒嗒")
-		mChange := systray.AddMenuItem("Change Me", "Change Me")
-		mChecked := systray.AddMenuItemCheckbox("Unchecked", "Check Me", true)
-		mEnabled := systray.AddMenuItem("Enabled", "Enabled")
-		// Sets the icon of a menu item. Only available on Mac.
-		mEnabled.SetTemplateIcon(icons.Data, icons.Data)
-
-		systray.AddMenuItem("Ignored", "Ignored")
-
-		subMenuTop := systray.AddMenuItem("SubMenuTop", "SubMenu Test (top)")
-		subMenuMiddle := subMenuTop.AddSubMenuItem("SubMenuMiddle", "SubMenu Test (middle)")
-		subMenuBottom := subMenuMiddle.AddSubMenuItemCheckbox("SubMenuBottom - Toggle Panic!", "SubMenu Test (bottom) - Hide/Show Panic!", false)
-		subMenuBottom2 := subMenuMiddle.AddSubMenuItem("SubMenuBottom - Panic!", "SubMenu Test (bottom)")
+		systray.SetTitle("GoGO!")
+		systray.SetTooltip("A great tool for developers.")
 
 		mUrl := systray.AddMenuItem("Open UI", "my home")
-		mQuit := systray.AddMenuItem("退出", "Quit the whole app")
-
-		// Sets the icon of a menu item. Only available on Mac.
-		mQuit.SetIcon(icons.Data)
-
-		systray.AddSeparator()
-		mToggle := systray.AddMenuItem("Toggle", "Toggle the Quit button")
-		shown := true
-		toggle := func() {
-			if shown {
-				subMenuBottom.Check()
-				subMenuBottom2.Hide()
-				mQuitOrig.Hide()
-				mEnabled.Hide()
-				shown = false
-			} else {
-				subMenuBottom.Uncheck()
-				subMenuBottom2.Show()
-				mQuitOrig.Show()
-				mEnabled.Show()
-				shown = true
-			}
-		}
 
 		for {
 			select {
-			case <-mChange.ClickedCh:
-				mChange.SetTitle("I've Changed")
-			case <-mChecked.ClickedCh:
-				if mChecked.Checked() {
-					mChecked.Uncheck()
-					mChecked.SetTitle("Unchecked")
-				} else {
-					mChecked.Check()
-					mChecked.SetTitle("Checked")
-				}
-			case <-mEnabled.ClickedCh:
-				mEnabled.SetTitle("Disabled")
-				mEnabled.Disable()
 			case <-mUrl.ClickedCh:
 				open.Run("http://127.0.0.1:9999")
-			case <-subMenuBottom2.ClickedCh:
-				panic("panic button pressed")
-			case <-subMenuBottom.ClickedCh:
-				toggle()
-			case <-mToggle.ClickedCh:
-				toggle()
-			case <-mQuit.ClickedCh:
-				systray.Quit()
-				fmt.Println("Quit2 now...")
-				return
 			}
 		}
 	}()
