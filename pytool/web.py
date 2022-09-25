4405443c30e3f19fd8f2fd53197836757bc5cb93
周志鹏 <823143047@qq.com>
2022-09-25 16:09:28 +0800

    print fix

--- pytool/web.py
+++ pytool/web.py
@@ -35,10 +35,30 @@ class Unbuffered(object):
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
 
+class IgnoreBrokenPipe(object):
+    def __init__(self, stream):
+        self.stream = stream
+        def ignore_einval(fn):
+            from functools import wraps
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+                try:
+                    return fn(*args, **kwargs)
+                except OSError as exc:
+                    if exc.errno != 22:
+                        raise exc
+                    else:  # mimicking the default SIGPIPE behavior on Windows
+                        sys.exit(1)
+            return wrapper
+
+        self.write = ignore_einval(lambda data: self.stream.write(data))
+        self.flush = ignore_einval(lambda: self.stream.flush())
 
 import sys
-sys.stdout = Unbuffered(sys.stdout)
-sys.stderr = Unbuffered(sys.stderr)
+# sys.stdout = Unbuffered(sys.stdout)
+# sys.stderr = Unbuffered(sys.stderr)
+sys.stdout = IgnoreBrokenPipe(sys.stdout)
+sys.stderr = IgnoreBrokenPipe(sys.stderr)
 
 import traceback
 
